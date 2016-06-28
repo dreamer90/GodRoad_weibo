879162cc7664bf7b45c8b728208a2513cb95f1f2
ld <dreamerjl@163.com>
2016-06-28 22:09:21 +0800

    创建项目－通神之路

---
+++ GodRoad_weibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GodRoad_weibo
+//
+//  Created by 李太白 on 16/6/28.
+//  Copyright © 2016年 李太白. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GodRoad_weiboTests/GodRoad_weiboTests.swift
@@ -0,0 +1,36 @@
+//
+//  GodRoad_weiboTests.swift
+//  GodRoad_weiboTests
+//
+//  Created by 李太白 on 16/6/28.
+//  Copyright © 2016年 李太白. All rights reserved.
+//
+
+import XCTest
+@testable import GodRoad_weibo
+
+class GodRoad_weiboTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
